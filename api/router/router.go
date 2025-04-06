656262c888f43d320f19d9c09ee58baa2760cb01
d-ishizuka <vbstkaksa.0716@gmail.com>
2025-04-06 12:24:28 +0900

    add task paths

--- api/router/router.go
+++ api/router/router.go
@@ -3,14 +3,14 @@ package router
 import (
 	"just_do_it/controller"
 	// "net/http"
-	// "os"
+	"os"
 
-	// "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	// "github.com/labstack/echo/v4/middleware"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 	// 	AllowOrigins: []string{"http://localhost:3001", os.Getenv("FE_URL")},
@@ -28,5 +28,16 @@ func NewRouter(uc controller.IUserController) *echo.Echo {
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
 	e.GET("/csrf", uc.CsrfToken)
+
+	t := e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token", //JWTトークンが格納されている場所
+	}))
+	t.GET("", tc.GetAllTasks)
+	t.GET("/:taskId", tc.GetTaskById)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:taskId", tc.UpdateTask)
+	t.DELETE("/:taskId", tc.DeleteTask)
 	return e
 }
